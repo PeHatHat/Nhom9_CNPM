1dfd06f84f94d6bb66f9baf90e9256e2619b0645
PeHatHat <phamhiangg2710@gmail.com>
2024-12-31 16:23:29 +0700

    merge

--- jewelry_auction/users/urls.py
+++ jewelry_auction/users/urls.py
@@ -1,16 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
 
 app_name = 'users'
-
-router = DefaultRouter()
-router.register(r'', views.UserViewSet)
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('', include(router.urls)),
 ]
